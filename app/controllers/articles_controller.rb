0cd05648672bb180213bdc92b7dc2416c2eda108
sandra <nyamweasandra@gmail.com>
2023-07-02 19:39:26 +0300

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,18 @@ class ArticlesController < ApplicationController
   end
 
   def show
+  session[:page_views] ||= 0  # Set initial value to 0 if session[:page_views] is nil
+  session[:page_views] += 1   # Increment page_views by 1 for each request
+
+  if session[:page_views] <= 3
+    # User has viewed 3 or fewer pages, render the article data
     article = Article.find(params[:id])
     render json: article
+  else
+    # User has viewed more than 3 pages, render an error message and unauthorized status
+    render json: { error: "You have reached the maximum page views limit." }, status: :unauthorized
   end
+end
 
   private
 
